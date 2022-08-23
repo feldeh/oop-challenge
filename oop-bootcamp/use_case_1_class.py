a7fa5d4bca12e855dfe6987b918f023ab1ca4dd5
fel <felicien.dehertogh@protonmail.com>
2022-08-24 00:50:14 +0200

    Create use case 1 - class

--- oop-bootcamp/use_case_1_class.py
+++ oop-bootcamp/use_case_1_class.py
@@ -1,21 +1,27 @@
 
+class Cart:
+    
+
 
 
 class Item:
-    def __init__(self, type: str, amount: int, price: float) -> None:
+    def __init__(self, name: str, type: str, amount: int, price: float) -> None:
+        self.name = name
         self.type = type
         self.amount = amount
         self.price = price
         
     def __str__(self) -> str:
-        return f"type: {self.type}, amount: {self.amount}, price: {self.price}"
+        return f"name: {self.name}, type: {self.type}, amount: {self.amount}, price: {self.price}"
     
     def total_price(self, type: str, amount: int, price: float, items: dict) -> float:
         total = 0
         for item in items:
             total += self.amount * self.price
         return total
-        
+
+
+
         
         
 basket = [Item("banana", 6, 1),
