b509ab3d455fb34ab1d7ca0c442c49df7eea8661
fel <felicien.dehertogh@protonmail.com>
2022-09-02 00:30:52 +0200

    add regex to extract instance type and __repr__ __str__

--- use_case_3.py
+++ use_case_3.py
@@ -1,3 +1,4 @@
+import re
 class Content:
     def __init__(self, original_title: str, text: str) -> None:
         self._original_title = original_title
@@ -9,6 +10,14 @@ class Article(Content):
     def __init__(self, original_title: str, text: str) -> None:
         super().__init__(original_title, text)
         self._updated_title = original_title
+        
+    def __str__(self) -> str:
+        instance_type = re.findall(r"\.(.*?)'", f"{type(self)}")[0]
+        return f'content type: "{instance_type}", original title: "{self.original_title}", new title: "{self.updated_title}", text: "{self.text}"'
+    
+    def __repr__(self) -> str:
+        instance_type = re.findall(r"\.(.*?)'", f"{type(self)}")[0]
+        return f'content type: "{instance_type}", original title: "{self.original_title}", new title: "{self.updated_title}", text: "{self.text}"'
         
     @property
     def original_title(self) -> str:
@@ -37,6 +46,14 @@ class Ads(Content):
         super().__init__(original_title, text)
         self._updated_title = original_title
         
+    def __str__(self) -> str:
+        instance_type = re.findall(r"\.(.*?)'", f"{type(self)}")[0]
+        return f'content type: "{instance_type}", original title: "{self.original_title}", new title: "{self.updated_title}", text: "{self.text}"'
+    
+    def __repr__(self) -> str:
+        instance_type = re.findall(r"\.(.*?)'", f"{type(self)}")[0]
+        return f'content type: "{instance_type}", original title: "{self.original_title}", new title: "{self.updated_title}", text: "{self.text}"'
+        
     @property
     def original_title(self) -> str:
         return self._original_title.upper()
@@ -62,6 +79,14 @@ class Vacancies(Content):
         super().__init__(original_title, text)
         self._updated_title = original_title
         
+    def __str__(self) -> str:
+        instance_type = re.findall(r"\.(.*?)'", f"{type(self)}")[0]
+        return f'content type: "{instance_type}", original title: "{self.original_title}", new title: "{self.updated_title}", text: "{self.text}"'
+    
+    def __repr__(self) -> str:
+        instance_type = re.findall(r"\.(.*?)'", f"{type(self)}")[0]
+        return f'content type: "{instance_type}", original title: "{self.original_title}", new title: "{self.updated_title}", text: "{self.text}"'
+        
     @property
     def original_title(self) -> str:
         return self._original_title + " - apply now!"
@@ -82,4 +107,13 @@ class Vacancies(Content):
     def text(self, value: str) -> None:
         self._text = value
         
+        
+vac = Vacancies("tt", "rr")
 
+art = Article("test", "test1")
+
+ads = Ads("abcd", "zwp")
+
+array = [art, vac, ads]
+
+print(array)
